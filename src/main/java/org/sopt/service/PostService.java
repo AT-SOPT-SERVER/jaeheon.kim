7b0224ade143b493b94d6c98c9b7e6498bd6b1c0
jher235 <tim668666@gmail.com>
2025-04-09 14:20:14 +0900

    feat: 이모지를 한글자로 취급하도록 함. - week1 여러 이모지를 이어붙이는 ‍‍를 판별하여 이게 존재할 경우 이어붙어진 이모지 개수를 제외하는 형식으로 구현함.

--- src/main/java/org/sopt/service/PostService.java
+++ src/main/java/org/sopt/service/PostService.java
@@ -31,7 +31,6 @@ public class PostService {
     }
 
     public Post getPost(Long id){
-        System.out.println(postRepository.findPostById(id).getTitle().length());
         return postRepository.findPostById(id);
     }
 
--- src/main/java/org/sopt/validator/PostValidator.java
+++ src/main/java/org/sopt/validator/PostValidator.java
@@ -1,15 +1,35 @@
 package org.sopt.validator;
 
-import org.sopt.domain.Post;
-import org.sopt.dto.request.PostRequest;
+import java.text.BreakIterator;
 
 public class PostValidator {
     public static final Integer MAX_TITLE_SIZE = 30;
+    public static final Character ZERO_WIDTH_JOINER = '\u200D';
 
     public static boolean isValidTitle(String title){
-        if (title.length() > MAX_TITLE_SIZE
-                || title.isBlank()){
+        if(title.isBlank()
+                || !isLessThanMaxTitleSize(title)){
             return false;
+        }
+        return true;
+    }
+
+
+    private static boolean isLessThanMaxTitleSize(String title){
+        BreakIterator textIterator = BreakIterator.getCharacterInstance();
+        textIterator.setText(title);
+        int cnt = 0;
+
+        while (textIterator.next() != BreakIterator.DONE){
+            char currentChar = textIterator.getText().current();
+            if (currentChar == ZERO_WIDTH_JOINER){
+                cnt--;
+                continue;
+            }
+            cnt++;
+            if(cnt > MAX_TITLE_SIZE){
+                return false;
+            }
         }
         return true;
     }
