f86a6fcfab6c932715f7c9540a2dbb94598380ef
jher235 <tim668666@gmail.com>
2025-04-19 20:04:08 +0900

    feat: 에러 로그 출력 기능 추가 - week2

--- src/main/java/org/sopt/exception/handler/ExceptionController.java
+++ src/main/java/org/sopt/exception/handler/ExceptionController.java
@@ -1,5 +1,7 @@
 package org.sopt.exception.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sopt.dto.ErrorResponseDto;
 import org.sopt.exception.CustomException;
 import org.springframework.http.HttpStatus;
@@ -7,17 +9,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
 @RestControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomException(CustomException exception) {
         HttpStatus httpStatus = resolveHttpStatus(exception);
+        writeLog(exception);
         return new ResponseEntity<>(ErrorResponseDto.from(exception), httpStatus);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleException(Exception exception) {
+        writeLog(exception);
         return new ResponseEntity<>(ErrorResponseDto.of(HttpStatus.INTERNAL_SERVER_ERROR.toString(), exception),
                 HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -27,5 +32,20 @@ public class ExceptionController {
                 .getErrorCode()
                 .getStatusCode()
                 .substring(0, 3)));
+    }
+
+    private void writeLog(CustomException e) {
+        Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+        logger.error("[{}]{}: {}",
+                e.getClass().getSimpleName(),
+                e.getErrorCode().getMessage(),
+                e.getDetail());
+    }
+
+    private void writeLog(Exception e) {
+        Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+        logger.error("[{}]: {}",
+                e.getClass().getSimpleName(),
+                e.getMessage());
     }
 }
