70ae1357aa7e6f277f5bf1c6bba45d121646cbd0
jher235 <tim668666@gmail.com>
2025-04-19 20:05:21 +0900

    feat: 생성자에 Validation 검사로직 추가 - week2 - Validation 의존성 없이 처리하려고 했는데,,, 이렇게 할 경우 Controller 단 이전에서 예외가 발생하므로 ExceptionController에서 처리하지 못하는 문제가 있음. 따라서 Validation 의존성 추가를 고민중

--- src/main/java/org/sopt/dto/request/post/PostRequest.java
+++ src/main/java/org/sopt/dto/request/post/PostRequest.java
@@ -1,4 +1,16 @@
 package org.sopt.dto.request.post;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.sopt.exception.BadRequestException;
+import org.sopt.exception.errorcode.ErrorCode;
+import org.sopt.validator.PostValidator;
+
 public record PostRequest(String title) {
+
+    public PostRequest(@JsonProperty(value = "title") String title) {
+        if (!PostValidator.isValidTitle(title)) {
+            throw new BadRequestException(ErrorCode.LENGTH);
+        }
+        this.title = title;
+    }
 }
