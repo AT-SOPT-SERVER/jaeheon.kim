7f3ebf4603fb5a81ce385025a9e2c2ff3c564d95
jher235 <tim668666@gmail.com>
2025-05-30 12:42:23 +0900

    feat: Comment 엔티티 작성 - week6-7

---
+++ src/main/java/org/sopt/domain/Comment.java
@@ -0,0 +1,30 @@
+package org.sopt.domain;
+
+import org.sopt.domain.base.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Comment extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 300, nullable = false)
+	private String content;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "post_id", nullable = false)
+	private Post post;
+}
