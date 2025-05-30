3617abbaae4acccdae57c0509e6dab4feb2b66b1
jher235 <tim668666@gmail.com>
2025-05-30 21:33:56 +0900

    feat: 테이블명을 users로 수정 - week6-7 - 테스트 환경에서 사용하는 h2의 경우 user 가 예약어이므로 users로 수정

--- src/main/java/org/sopt/domain/User.java
+++ src/main/java/org/sopt/domain/User.java
@@ -1,54 +1,61 @@
 package org.sopt.domain;
 
-import jakarta.persistence.*;
-import org.sopt.domain.base.BaseEntity;
-
 import java.util.Objects;
 
+import org.sopt.domain.base.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
+@Table(name = "users")
 public class User extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false)
-    private String name;
-    private String email;
+	@Column(nullable = false)
+	private String name;
+	private String email;
 
-    protected User() {
-    }
+	protected User() {
+	}
 
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof User that)) {
-            return false;
-        }
-        return Objects.equals(this.getId(), that.getId());
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof User that)) {
+			return false;
+		}
+		return Objects.equals(this.getId(), that.getId());
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.getId());
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.getId());
+	}
 }
