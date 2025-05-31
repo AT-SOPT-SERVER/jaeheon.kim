23eb0be72ba3d1b40da2c061abab25a0baed72a9
jher235 <tim668666@gmail.com>
2025-05-31 23:13:11 +0900

    test: 댓글 좋아요 생성 테스트 작성 - week6-7

--- src/test/java/org/sopt/service/like/LikeServiceTest.java
+++ src/test/java/org/sopt/service/like/LikeServiceTest.java
@@ -8,12 +8,14 @@ import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.sopt.domain.Comment;
 import org.sopt.domain.Like;
 import org.sopt.domain.Post;
 import org.sopt.domain.User;
 import org.sopt.exception.ConflictException;
 import org.sopt.exception.NotFoundException;
 import org.sopt.exception.errorcode.ErrorCode;
+import org.sopt.repository.CommentRepository;
 import org.sopt.repository.LikeRepository;
 import org.sopt.repository.UserRepository;
 import org.sopt.repository.post.PostRepository;
@@ -34,10 +36,13 @@ class LikeServiceTest {
 	private PostRepository postRepository;
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private CommentRepository commentRepository;
 
 	@AfterEach
 	public void afterTest() {
 		likeRepository.deleteAllInBatch();
+		commentRepository.deleteAllInBatch();
 		postRepository.deleteAllInBatch();
 		userRepository.deleteAllInBatch();
 	}
@@ -129,6 +134,55 @@ class LikeServiceTest {
 			.satisfies(e -> {
 				NotFoundException notFoundException = (NotFoundException)e;
 				assertThat(notFoundException.getErrorCode()).isEqualTo(ErrorCode.POST_LIKE_NOT_FOUND);
+			});
+	}
+
+	@DisplayName("좋아요를 누르지 않은 댓글에 좋아요를 성공적으로 생성함")
+	@Test
+	void createCommentLike() {
+		// given
+		User user = new User("userA", "email");
+		User userA = userRepository.save(user);
+
+		Post post = new Post(user, "postA", "contentA", null);
+		Post postA = postRepository.save(post);
+
+		Comment comment = new Comment(postA, userA, "commentA");
+		Comment commentA = commentRepository.save(comment);
+
+		// when
+		likeService.addCommentLike(commentA.getId(), userA.getId());
+
+		// then
+		assertThat(likeRepository.findAll()).hasSize(1)
+			.extracting("id.userId", "id.contentId", "id.contentType")
+			.contains(
+				tuple(userA.getId(), commentA.getId(), COMMENT)
+			);
+	}
+
+	@DisplayName("이미 유저의 좋아요가 존재하는 댓글에 좋아요 생성 요청을 시도할 경우 예외가 발생함")
+	@Test
+	void createDuplicatedCommentLike() {
+		// given
+		User user = new User("userA", "email");
+		User userA = userRepository.save(user);
+
+		Post post = new Post(user, "postA", "contentA", null);
+		Post postA = postRepository.save(post);
+
+		Comment comment = new Comment(postA, userA, "commentA");
+		Comment commentA = commentRepository.save(comment);
+
+		likeRepository.save(Like.createLike(user, commentA));
+
+		assertThat(likeRepository.findAll()).hasSize(1);
+		// when & then
+		assertThatThrownBy(() -> likeService.addCommentLike(commentA.getId(), userA.getId()))
+			.isInstanceOf(ConflictException.class)
+			.satisfies(e -> {
+				ConflictException conflictException = (ConflictException)e;
+				assertThat(conflictException.getErrorCode()).isEqualTo(ErrorCode.COMMENT_ALREADY_LIKED);
 			});
 	}
 
