88d74a8eca56a4e0d0d8d8730df3024e03003930
jher235 <tim668666@gmail.com>
2025-05-31 12:20:10 +0900

    test: 댓글 수정 테스트 작성 - week6-7

--- src/test/java/org/sopt/service/comment/CommentServiceTest.java
+++ src/test/java/org/sopt/service/comment/CommentServiceTest.java
@@ -2,6 +2,9 @@ package org.sopt.service.comment;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +12,8 @@ import org.sopt.domain.Comment;
 import org.sopt.domain.Post;
 import org.sopt.domain.User;
 import org.sopt.dto.request.comment.CommentCreateRequest;
+import org.sopt.dto.request.comment.CommentUpdateRequest;
+import org.sopt.exception.ForbiddenException;
 import org.sopt.repository.CommentRepository;
 import org.sopt.repository.UserRepository;
 import org.sopt.repository.post.PostRepository;
@@ -71,6 +76,52 @@ class CommentServiceTest {
 
 		assertThat(result).isNotNull();
 		assertThat(result.getContent()).isEqualTo("commentA");
+	}
+
+	@DisplayName("작성자가 자신의 댓글 수정을 시도할 경우 정상적으로 수정됨")
+	@Test
+	void updateCommentSameUser() {
+		// given
+		User user = new User("userA", "email");
+		User userA = userRepository.save(user);
+
+		Post post = new Post(user, "postA", "contentA", null);
+		Post postA = postRepository.save(post);
+
+		CommentCreateRequest request = new CommentCreateRequest("commentA");
+		Comment commentA = commentService.createPostComment(postA.getId(), userA.getId(), request);
+
+		String updatedContent = "updatedA";
+		CommentUpdateRequest updateRequest = new CommentUpdateRequest(Optional.of(updatedContent));
+		// when
+		commentService.updatePostComment(commentA.getId(), userA.getId(), updateRequest);
+
+		// then
+		Comment result = commentRepository.findById(commentA.getId()).get();
+		assertThat(result.getContent()).isEqualTo(updatedContent);
+	}
+
+	@DisplayName("유저가 자신이 작성하지 않은 댓글을 수정하려는 경우 예외가 발생함")
+	@Test
+	void updateCommentOtherUser() {
+		// given
+		User userA = new User("userA", "email");
+		User userB = new User("userB", "email");
+		userRepository.saveAll(List.of(userA, userB));
+
+		Post post = new Post(userA, "postA", "contentA", null);
+		Post postA = postRepository.save(post);
+
+		CommentCreateRequest request = new CommentCreateRequest("commentA");
+		Comment commentA = commentService.createPostComment(postA.getId(), userA.getId(), request);
+
+		String updatedContent = "updatedA";
+		CommentUpdateRequest updateRequest = new CommentUpdateRequest(Optional.of(updatedContent));
+		// when & then
+		assertThatThrownBy(() -> commentService.updatePostComment(commentA.getId(), userB.getId(), updateRequest))
+			.isInstanceOf(ForbiddenException.class)
+			.hasFieldOrProperty("errorCode");
+
 	}
 
 }
