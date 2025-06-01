fb389b6688672f25318cca4609c435e50eafe570
jher235 <tim668666@gmail.com>
2025-06-01 19:53:34 +0900

    test: 좋아요 추가 시 동시성 테스트 작성 - week6-7

--- src/test/java/org/sopt/service/like/LikeServiceTest.java
+++ src/test/java/org/sopt/service/like/LikeServiceTest.java
@@ -3,7 +3,11 @@ package org.sopt.service.like;
 import static org.assertj.core.api.Assertions.*;
 import static org.sopt.domain.enums.ContentType.*;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -54,7 +58,7 @@ class LikeServiceTest {
 		User user = new User("userA", "email");
 		User userA = userRepository.save(user);
 
-		Post post = new Post(user, "postA", "contentA", null);
+		Post post = Post.createNew(user, "postA", "contentA");
 		Post postA = postRepository.save(post);
 
 		// when
@@ -68,6 +72,9 @@ class LikeServiceTest {
 			.contains(
 				tuple(userA.getId(), postA.getId(), POST)
 			);
+
+		assertThat(postRepository.findById(postA.getId()).get().getLikeCount())
+			.isEqualTo(1);
 
 		// assertThat(result.get(0).getId())
 		// 	.extracting("userId", "contentId", "contentType")
@@ -83,7 +90,7 @@ class LikeServiceTest {
 		User user = new User("userA", "email");
 		User userA = userRepository.save(user);
 
-		Post post = new Post(user, "postA", "contentA", null);
+		Post post = Post.createNew(user, "postA", "contentA");
 		Post postA = postRepository.save(post);
 
 		likeService.addPostLike(postA.getId(), userA.getId());
@@ -103,7 +110,7 @@ class LikeServiceTest {
 		User user = new User("userA", "email");
 		User userA = userRepository.save(user);
 
-		Post post = new Post(user, "postA", "contentA", null);
+		Post post = Post.createNew(user, "postA", "contentA");
 		Post postA = postRepository.save(post);
 
 		Like likeA = likeRepository.save(Like.createLike(userA, postA));
@@ -125,7 +132,7 @@ class LikeServiceTest {
 		User user = new User("userA", "email");
 		User userA = userRepository.save(user);
 
-		Post post = new Post(user, "postA", "contentA", null);
+		Post post = Post.createNew(user, "postA", "contentA");
 		Post postA = postRepository.save(post);
 
 		// when & then
@@ -144,7 +151,7 @@ class LikeServiceTest {
 		User user = new User("userA", "email");
 		User userA = userRepository.save(user);
 
-		Post post = new Post(user, "postA", "contentA", null);
+		Post post = Post.createNew(user, "postA", "contentA");
 		Post postA = postRepository.save(post);
 
 		Comment comment = new Comment(postA, userA, "commentA");
@@ -168,7 +175,7 @@ class LikeServiceTest {
 		User user = new User("userA", "email");
 		User userA = userRepository.save(user);
 
-		Post post = new Post(user, "postA", "contentA", null);
+		Post post = Post.createNew(user, "postA", "contentA");
 		Post postA = postRepository.save(post);
 
 		Comment comment = new Comment(postA, userA, "commentA");
@@ -193,7 +200,7 @@ class LikeServiceTest {
 		User user = new User("userA", "email");
 		User userA = userRepository.save(user);
 
-		Post post = new Post(user, "postA", "contentA", null);
+		Post post = Post.createNew(user, "postA", "contentA");
 		Post postA = postRepository.save(post);
 
 		Comment comment = new Comment(postA, userA, "commentA");
@@ -217,7 +224,7 @@ class LikeServiceTest {
 		User user = new User("userA", "email");
 		User userA = userRepository.save(user);
 
-		Post post = new Post(user, "postA", "contentA", null);
+		Post post = Post.createNew(user, "postA", "contentA");
 		Post postA = postRepository.save(post);
 
 		Comment comment = new Comment(postA, userA, "commentA");
@@ -230,6 +237,56 @@ class LikeServiceTest {
 				NotFoundException notFoundException = (NotFoundException)e;
 				assertThat(notFoundException.getErrorCode()).isEqualTo(ErrorCode.COMMENT_LIKE_NOT_FOUND);
 			});
+	}
+
+	/**
+	 * 최초 addPostLike의 동시성을 고려하지 않았을 경우 좋아요 수는 100개의 요청에 대해 10~11개만 증가하여 테스트를 실패했었음.
+	 * 고려한 방법은 아래와 같은데
+	 * 낙관적 락 -> 실패가 자주 발생하고 이걸 폴링으로 재시도 처리할 경우 자원 낭비가 심함.
+	 * 비관적 락 -> likeCount 하나 증가를 위해서 해당 레코드의 락을 잡는 것도 너무 비용이 많이 듦
+	 * redis 같은 걸로 모아서 처리 -> 솔직히 좋아보여서 추후 이 방법을 사용하고 싶지만, 현재는 redis를 사용하지 않을 예정이므로 패스
+	 * 증감 쿼리 -> 현재 채택한 방법, 기본적으로 RDB 는 CA 이므로 대부분 원자성을 보장하지만,,, DB 마다 다를 수 있으므로 조심해야 함.
+	 */
+	@DisplayName("유저 100명이 동시에 좋아요를 누르는 경우 레이스 컨디션이 발생하지 않고 정상적으로 likeCount 가 100이 됨")
+	@Test
+	void concurrentPostLike() throws InterruptedException {
+		// given
+
+		int requestCount = 100;
+
+		ExecutorService executorService = Executors.newFixedThreadPool(requestCount);
+
+		CountDownLatch countDownLatch = new CountDownLatch(requestCount);
+
+		List<User> users = new LinkedList<>();
+
+		for (int i = 0; i < requestCount; i++) {
+			users.add(new User("user" + i, "email" + i));
+		}
+
+		userRepository.saveAll(users);
+
+		Post post = Post.createNew(users.get(0), "postA", "contentA");
+		Post postA = postRepository.save(post);
+
+		// when
+		for (User user : users) {
+			executorService.submit(() -> {
+				try {
+					likeService.addPostLike(postA.getId(), user.getId());
+				} finally {
+					countDownLatch.countDown();
+				}
+			});
+		}
+
+		countDownLatch.await();
+		executorService.shutdown();
+
+		// then
+		assertThat(likeRepository.findAll()).hasSize(requestCount);
+		assertThat(postRepository.findById(postA.getId()).get().getLikeCount())
+			.isEqualTo(requestCount);
 	}
 
 }
